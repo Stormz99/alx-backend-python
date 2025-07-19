885a26894fe1354147b9773d8e702d3fd4394f3f
Abiodun <ijiolastormz@gmail.com>
2025-07-19 22:17:41 +0100

    Working on Unit test

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""This module contains unit tests for the `access_nested_map` function
+in the utils module.
+"""
+
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    Unit test class for the `access_nested_map` function in utils.py.
+    Test standard inputs and nested dictionary traversal.
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    
+    def test_access_nested_map( 
+        self,
+        nested_map: dict,
+        path: tuple,
+        expected: object
+    ) -> None:
+        """
+        Test that access_nested_map returns correct value
+        for given nested map and key path.
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+        
+if __name__ == "__main__":
+    unittest.main()
