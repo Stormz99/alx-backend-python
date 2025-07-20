d5557efa12cb9e67e44425faf227a7c89fa32864
Abiodun <ijiolastormz@gmail.com>
2025-07-20 21:06:54 +0100

    Add DRF config and register chats app

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -130,13 +130,4 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.UserRateThrottle',
-        'rest_framework.throttling.AnonRateThrottle',
-    ],
-    'DEFAULT_THROTTLE_RATES': {
-        'user': '1000/day',
-        'anon': '100/day',
-    },
 }
-
