6f8a3e87324b68e48bd1142e2a2c94f260d4596d
Abiodun <ijiolastormz@gmail.com>
2025-08-03 19:33:11 +0100

    Fix: Timezone added to Signals

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -22,3 +22,6 @@ class MessageHistory(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE)
     old_content = models.TextField()
     edited_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"History for {self.message.id}"
